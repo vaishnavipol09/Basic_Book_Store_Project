8e6c0ffe656f681f40c3dbd1091ab07e09a2c7a9
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-16 18:53:58 +0530

    aggregation method

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -17,12 +17,16 @@ def index(request):
     })
 
 
-def book_detail(request, slug):
-   # book = Book.objects.get(pk=id)   #pk means primary key
-    book = get_object_or_404(Book,slug=slug)
-    return render(request, 'book_outlet/book_detail.html' , {
+def book_detail(request,slug):
+    # try:
+    #  book = Book.objects.get(pk=id)
+    
+    # except: 
+    #  raise Http404()
+    book = get_object_or_404(Book, slug=slug)
+    return render(request ,"book_outlet/book_detail.html",{
         "title" : book.title,
         "author" : book.author,
         "rating" : book.rating,
-        "is_bestselling" : book.is_bestselling,
-    })
+        "is_best_seller" :book.is_best_selling
+    })       
