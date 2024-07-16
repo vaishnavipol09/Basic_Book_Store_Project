d6a3aa12af375cabd083d8f5b05b9a57d1f40e3f
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-16 18:31:49 +0530

    aggregation method

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import get_object_or_404, render # type: ignore
 
 from .models import Book
+from django.db.models import Avg # type: ignore
 
 # Create your views here.
 
 def index(request):
-        books = Book.objects.all()
+        books = Book.objects.all().order_by("-title") # in decending order
+        num_books = books.count()
+        avg_rating = books.aggregate(Avg("rating"))
+
         return render(request, 'book_outlet/index.html' , {
-        "books": books
+        "books": books,
+        "total_numbers_of_books" : num_books,
+        "average_rating" : avg_rating
     })
 
 
-def book_detail(request, id):
-    book = Book.objects.get(pk=id)   #pk means primary key
+def book_detail(request, slug):
+   # book = Book.objects.get(pk=id)   #pk means primary key
+    book = get_object_or_404(Book,slug=slug)
     return render(request, 'book_outlet/book_detail.html' , {
         "title" : book.title,
         "author" : book.author,
